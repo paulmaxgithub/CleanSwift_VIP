3ff0e86f3e5d5d3ee8000087d7bb1e991afa8a54
paulmaxgithub <ipaulmax@icloud.com>
2024-01-06 02:25:11 +0300

    What to test:
    (Pull request builds now use a beta app icon)
    
    1. Added new icon for beta testing
    2. Created CI script that changes the icon of the main app to the beta version icon

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  IceCreamCreator
+#
+#  Created by PAULMAX iOS DEV on 06/01/2024.
+#  Copyright © 2024 Ray Wenderlich. All rights reserved.
+
+if [[ -n $CI_PULL_REQUEST_NUMBER && $CI_XCODEBUILD_ACTION = 'archive' ]];
+then
+    echo "Setting IceCreamCreator Beta App Icon"
+    APP_ICON_PATH=$CI_WORKSPACE/Resources/Assets.xcassets/AppIcon.appiconset
+    
+    # Remove existing App Icon
+    rm -rf $APP_ICON_PATH
+    
+    # Replace with Fruta Beta App Icon
+    mv "$CI_WORKSPACE/ci_scripts/AppIcon-Beta.appiconset" $APP_ICON_PATH
+fi
