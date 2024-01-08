b0e942c28ae5b978958e8c22726fc4bad5ba2fbe
paulmaxgithub <ipaulmax@icloud.com>
2024-01-09 00:45:46 +0300

    New Build with Fixed Code:
    1. Redirected a path to files
    2. info.plist has changed a own order

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -9,11 +9,11 @@
 if [[ -n $CI_PULL_REQUEST_NUMBER && $CI_XCODEBUILD_ACTION = 'archive' ]];
 then
     echo "Setting IceCreamCreator Beta App Icon"
-    APP_ICON_PATH=$CI_PRIMARY_REPOSITORY_PATH/Resources/Assets.xcassets/AppIcon.appiconset
+    APP_ICON_PATH=$CI_PRIMARY_REPOSITORY_PATH/IceCreamCreator/Resources/Assets.xcassets/AppIcon.appiconset
     
     # Remove existing App Icon
     rm -rf $APP_ICON_PATH
     
-    # Replace with Fruta Beta App Icon
+    # Replace with IceCreamCreator Beta App Icon
     mv "$CI_PRIMARY_REPOSITORY_PATH/ci_scripts/AppIcon-Beta.appiconset" $APP_ICON_PATH
 fi
